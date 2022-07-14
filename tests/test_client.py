c6bef95c406fb48c09526625e819b96adcf73b50
nitinpandey01 <106578178+nitinpandey01@users.noreply.github.com>
2022-07-14 16:31:26 +0530

    deleted tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -60,13 +60,6 @@ class TodolistClientTestCase(TestCase):
         self.assert_200(response)
         self.assert_template_used("login.html")
 
-    def test_overview_page(self):
-        self.register_and_login(self.username_alice)
-        response = self.client.get(url_for("main.todolist_overview"))
-        # expect not redirect as user is logged in
-        self.assert_200(response)
-        self.assert_template_used("overview.html")
-
     def test_last_seen_update_after_login(self):
         self.register_user(self.username_alice)
         user = User.query.filter_by(username=self.username_alice).first()
@@ -74,20 +67,3 @@ class TodolistClientTestCase(TestCase):
         self.login_user(self.username_alice)
         after = user.last_seen
         self.assertNotEqual(before, after)
-
-    def test_register_and_login_and_logout(self):
-        # register a new account
-        response = self.register_user(self.username_alice)
-        # expect redirect to login
-        self.assert_redirects(response, "/auth/login")
-
-        # login with the new account
-        response = self.login_user(self.username_alice)
-        # expect redirect to index
-        self.assert_redirects(response, "/")
-
-        # logout
-        response = self.client.get(url_for("auth.logout"), follow_redirects=True)
-        # follow redirect to index
-        self.assert_200(response)
-        self.assert_template_used("index.html")
